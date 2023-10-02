ed979371c1e8866cb9d90f4dbbd3d8060899d84b
Dawid Strzyż <35563213+Streyter@users.noreply.github.com>
2023-10-02 17:10:43 +0200

    Add DataSeeder
    Data added for postcards(PostcardData, Postcard) and user(User, UserDetail, UserFriend, UserPostcard, UserStat, Address)

--- Infrastructure/Data/Seeder/UserSeeder.cs
+++ Infrastructure/Data/Seeder/UserSeeder.cs
@@ -8,33 +8,204 @@ public class UserSeeder
     {
         IEnumerable<User> users = new List<User>()
         {
-            new User()
-            {
-                NickName = "TestUser",
-                Email = "Test@email.com",
-                // string123
-                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
-                Role = "USER",
-                CreatedAt = DateTime.Now,
-            },
-            new User()
+            new User()// ID: 1
             {
                 NickName = "AdminUser",
-                Email = "Admin@email.com",
+                Email = "admin@email.com",
                 // string123
                 Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
                 Role = "ADMIN",
                 CreatedAt = DateTime.Now,
             },
-            new User()
+            new User()// ID: 2
             {
-                NickName = "admin2",
+                NickName = "Admin2",
                 Email = "admin2@email.com",
                 // string123
                 Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
                 Role = "ADMIN",
                 CreatedAt = DateTime.Now,
-            }
+            },
+            new User()// ID: 3
+            {
+                NickName = "Oltaracel",
+                Email = "john.doe@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 4
+            {
+                NickName = "Tananicol",
+                Email = "jane.smith@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 5
+            {
+                NickName = "Suel",
+                Email = "mike.johnson@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 6
+            {
+                NickName = "Valance",
+                Email = "sarah.brown@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 7
+            {
+                NickName = "DavDavoo",
+                Email = "david.wilson@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 8
+            {
+                NickName = "Charllia",
+                Email = "emily.taylor@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 9
+            {
+                NickName = "Dan",
+                Email = "daniel.davis@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 10
+            {
+                NickName = "Oxonomy",
+                Email = "olivia.clark@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 11
+            {
+                NickName = "Protesian",
+                Email = "michael.martinez@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 12
+            {
+                NickName = "Robotik",
+                Email = "sophia.lewis@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 13
+            {
+                NickName = "Gigadude",
+                Email = "william.brown@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 14
+            {
+                NickName = "Slyrack",
+                Email = "ava.johnson@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 15
+            {
+                NickName = "Kerplunk",
+                Email = "james.smith@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 16
+            {
+                NickName = "Synnetan",
+                Email = "emma.wilson@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 17
+            {
+                NickName = "Wrian",
+                Email = "daniel.jones@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 18
+            {
+                NickName = "Boone",
+                Email = "oliver.smith@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 19
+            {
+                NickName = "Cothurnal",
+                Email = "sophia.davis@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 20
+            {
+                NickName = "NarcCop",
+                Email = "matthew.johnson@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 21
+            {
+                NickName = "Octopi",
+                Email = "amelia.clark@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
+            new User()// ID: 22
+            {
+                NickName = "Capitulation",
+                Email = "ethan.taylor@email.com",
+                // string123
+                Password = "AQAAAAIAAYagAAAAEF/Vbi6W4FbQcd9TmJKKTjB6gGpNb72wUr36gdM71vDOWrsdNRTGVkUMyjIJr979FA==",
+                Role = "USER",
+                CreatedAt = DateTime.Now,
+            },
         };
         return users;
     }
---
+++ Infrastructure/Data/Seeder/UserStatSeeder.cs
@@ -0,0 +1,146 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Data.Seeder;
+
+public class UserStatSeeder
+{
+    public static IEnumerable<UserStat> GetUsersSeeder()
+    {
+        IEnumerable<UserStat> userStats = new List<UserStat>()
+        {
+            new UserStat()// ID: 1
+            {
+                PostcardsSent = 1,
+                PostcardsReceived = 2,
+                Score = 3,
+            },
+            new UserStat()// ID: 2
+            {
+                PostcardsSent = 3,
+                PostcardsReceived = 2,
+                Score = 5,
+            },
+            new UserStat()// ID: 3
+            {
+                PostcardsSent = 4,
+                PostcardsReceived = 3,
+                Score = 7,
+            },
+            new UserStat()// ID: 4
+            {
+                PostcardsSent = 5,
+                PostcardsReceived = 2,
+                Score = 7,
+            },
+            new UserStat()// ID: 5
+            {
+                PostcardsSent = 2,
+                PostcardsReceived = 6,
+                Score = 8,
+            },
+             new UserStat()// ID: 6
+            {
+                PostcardsSent = 4,
+                PostcardsReceived = 2,
+                Score = 6,
+            },
+            new UserStat()// ID: 7
+            {
+                PostcardsSent = 6,
+                PostcardsReceived = 7,
+                Score = 13,
+            },
+            new UserStat()// ID: 8
+            {
+                PostcardsSent = 3,
+                PostcardsReceived = 7,
+                Score = 10,
+            },
+            new UserStat()// ID: 9
+            {
+                PostcardsSent = 7,
+                PostcardsReceived = 5,
+                Score = 12,
+            },
+            new UserStat()// ID: 10
+            {
+                PostcardsSent = 2,
+                PostcardsReceived = 4,
+                Score = 6,
+            },
+            new UserStat()// ID: 11
+            {
+                PostcardsSent = 1,
+                PostcardsReceived = 7,
+                Score = 8,
+            },
+            new UserStat()// ID: 12
+            {
+                PostcardsSent = 5,
+                PostcardsReceived = 3,
+                Score = 8,
+            },
+            new UserStat()// ID: 13
+            {
+                PostcardsSent = 7,
+                PostcardsReceived = 6,
+                Score = 13,
+            },
+            new UserStat()// ID: 14
+            {
+                PostcardsSent = 3,
+                PostcardsReceived = 4,
+                Score = 7,
+            },
+            new UserStat()// ID: 15
+            {
+                PostcardsSent = 6,
+                PostcardsReceived = 4,
+                Score = 10,
+            },
+            new UserStat()// ID: 16
+            {
+                PostcardsSent = 4,
+                PostcardsReceived = 6,
+                Score = 10,
+            },
+            new UserStat()// ID: 17
+            {
+                PostcardsSent = 3,
+                PostcardsReceived = 5,
+                Score = 8,
+            },
+            new UserStat()// ID: 18
+            {
+                PostcardsSent = 2,
+                PostcardsReceived = 7,
+                Score = 9,
+            },
+            new UserStat()// ID: 19
+            {
+                PostcardsSent = 7,
+                PostcardsReceived = 3,
+                Score = 10,
+            },
+            new UserStat()// ID: 20
+            {
+                PostcardsSent = 1,
+                PostcardsReceived = 5,
+                Score = 6,
+            },
+            new UserStat()// ID: 21
+            {
+                PostcardsSent = 5,
+                PostcardsReceived = 1,
+                Score = 6,
+            },
+            new UserStat()// ID: 22
+            {
+                PostcardsSent = 6,
+                PostcardsReceived = 2,
+                Score = 8,
+            },
+        };
+        return userStats;
+    }
+}
