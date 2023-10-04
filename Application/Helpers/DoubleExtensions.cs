23a0067e6752dba3507e13a9db5c6a637f61e57e
Dawid Strzyż <35563213+Streyter@users.noreply.github.com>
2023-10-05 01:44:51 +0200

    Create current location response

---
+++ Application/Helpers/DoubleExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Helpers;
+public static class DoubleExtensions
+{
+    public static double ToDouble(this string input)
+    {
+        CultureInfo[] cultures = new CultureInfo[]
+        {
+            CultureInfo.InvariantCulture,
+            new CultureInfo("fr-FR"),
+            new CultureInfo("de-DE"),
+            new CultureInfo("pl-PL")
+        };
+
+        foreach (CultureInfo culture in cultures)
+        {
+            if (double.TryParse(input, NumberStyles.Float, culture, out double result))
+            {
+                return result;
+            }
+        }
+
+        throw new FormatException("Input string is not in a valid numeric format.");
+    }
+}
+
